e0221322e829ad86905f8f22f897bb626295b73b
Tulio Roberto <tuliorobert2@gmail.com>
2022-01-25 14:45:01 -0300

    Create and implement classes 'Bootcamp' and 'Dev'

---
+++ src/desafioDominio/Dev.java
@@ -0,0 +1,61 @@
+package desafioDominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudoIncritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {}
+
+    public void progredir() {}
+
+    public void calcularTotalXp() {}
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudoIncritos() {
+        return this.conteudoIncritos;
+    }
+
+    public void setConteudoIncritos(Set<Conteudo> conteudoIncritos) {
+        this.conteudoIncritos = conteudoIncritos;
+    }
+
+    public Set<Conteudo> getConteudoConcluidos() {
+        return this.conteudoConcluidos;
+    }
+
+    public void setConteudoConcluidos(Set<Conteudo> conteudoConcluidos) {
+        this.conteudoConcluidos = conteudoConcluidos;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Dev)) {
+            return false;
+        }
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudoIncritos, dev.conteudoIncritos) && Objects.equals(conteudoConcluidos, dev.conteudoConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudoIncritos, conteudoConcluidos);
+    }
+
+
+}
