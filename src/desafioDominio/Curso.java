a529e0192a2a7cf4c3f9839d943941d6cc9e97a3
Tulio Roberto <tuliorobert2@gmail.com>
2022-01-25 14:27:52 -0300

    Create and implement class 'Conteudo'

--- src/desafioDominio/Curso.java
+++ src/desafioDominio/Curso.java
@@ -1,24 +1,11 @@
 package desafioDominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return this.titulo;
-    }
-
-    public void setTitulo(String titulo)  {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return this.descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
